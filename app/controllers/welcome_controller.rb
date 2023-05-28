0d5c98363feaa5f6f7243ea36930470d753a530f
marcoshenke <marcos.henke3@gmail.com>
2023-05-28 17:05:52 -0300

    83. Cookies e Sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = 'Curso de Ruby on Rails [cookie]'
+    session[:curso] = 'Curso de Ruby on Rails [session]'
     @nome = params[:nome]
     @curso = params[:curso]
   end
