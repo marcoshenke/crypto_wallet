4acda41d21c835b0b02e9e2ded3a65e069c2e725
marcoshenke <marcos.henke3@gmail.com>
2023-05-29 17:11:50 -0300

    84. Usando a aplicação vários idiomas em tempo de execução

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    cookies[:locale] = params[:locale] if params[:locale]
+
+    return unless cookies[:locale]
+    return unless I18n.locale != cookies[:locale]
+
+    I18n.locale = cookies[:locale]
+  end
 end
